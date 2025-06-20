36a6258cc106d0564f778a7ffa6d41d4b87dbf43
JuanLopez2004 <juanlopeze22004@gmail.com>
2025-06-20 16:57:08 -0500

    page sig information and new temp images in public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,18 @@ export default function Home() {
       <h1>acm</h1>
 
       <SigCard
-        name="Juan"
-        description="A software engineer with a passion for building scalable applications."
-        image_url="https://avatars.githubusercontent.com/u/123456789?v=4"
+        name="SIGmainframe"
+        description="SIGmainframe is our deep dive into the world of enterprise computing,
+         especially focused on IBM mainframes.
+         Whether you're curious about what powers the world’s banking and airline systems,
+         or you want to earn IBM Z Developer Badges, this is the SIG for you!"
+        image_url="/IBM_z16_.png" //these are just placeholder images that fit the vibe. find better later
       />
       <SigCard
-        name="Boltium Fortnitum"
-        description="A fictional character known for his expertise in fortnite strategies."
-        image_url="https://avatars.githubusercontent.com/u/987654321?v=4"
+        name="SIGwebdev"
+        description="SIGwebdev is for anyone interested in building things on the web—from total beginners to experienced devs.
+         If you're into HTML/CSS, PHP, React, Tailwind, or just want to make your portfolio shine, this is your space"
+        image_url="/webdev.png" //these are just placeholder images that fit the vibe. find better later
         flipped={true} // Example of using the flipped prop
       />
 
