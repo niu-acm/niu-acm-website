891d308234e1ad0baee176ef2b4888ab5c2d13bf
djj04 <dougjj04@gmail.com>
2025-06-21 01:48:09 -0500

    hello

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,22 +1,35 @@
+import FooterIcon from './footericon';
+
 export default function Footer() {
     return (
        <>
         <style>{`
-            
             .footer {
+               display: flex;
                align-items: center;
-               justify-content: space-between;
+               justify-content: center;
+               background-color:rgb(200, 16, 46);
+               color: white;
+               border-radius: 10px 10px 0px 0px;
+               flex-direction: row;
+            }
 
+            .icons-wrapper {
+               display: flex;
+               flex-direction: row;
+               gap: 50px;
+               margin-top: 15px;
+               margin-bottom: 15px;
             }
         `}</style>
 
         <footer className="footer">
-            <h1>Connect With Us!</h1>
-            <div>
-                <a href="mailto:niuhuskiesacm@gmail.com">
-                    <img src="https://pixabay.com/images/search/email/" alt="Email Icon"></img>
-                </a>
-                <a href="mailto:niuhuskiesacm@gmail.com">Send us an Email</a>
+            <div className="icons-wrapper">
+                <FooterIcon iconName="Email Us" link="mailto:niuhuskiesacm@gmail.com" iconPath="icons/mail.svg"/>
+                <FooterIcon iconName="Discord" link="https://discord.gg/gzQDaQMGjJ" iconPath="icons/brand-discord.svg"/>
+                <FooterIcon iconName="Huskie Hub" link="https://huskiehub.niu.edu/NIUACM/club_signup" iconPath="icons/heading.svg"/>
+                <FooterIcon iconName="GitHub" link="https://github.com/niu-acm" iconPath="icons/brand-github.svg"/>
+                <FooterIcon iconName="LinkedIn" link="https://www.linkedin.com/groups/14721661" iconPath="icons/brand-linkedin.svg"/>
             </div>
         </footer>
         </>
---
+++ src/app/footericon.tsx
@@ -0,0 +1,48 @@
+type FooterIconProps = {
+    iconName: string;
+    link: string;
+    iconPath: string;
+};
+
+export default function FooterIcon({
+    iconName,
+    link,
+    iconPath
+}: FooterIconProps) {
+    return (
+        <>
+        <style>{`
+
+            .icon-button {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                gap: 8px;
+                padding: 10px 20px 10px 20px;
+                border-radius: 10px;
+            }
+
+            .icons {
+                height: 30px;
+                width: auto;
+                color: white;
+                -webkit-filter: invert(100%);
+            }
+
+            .icon-button:hover {
+                background-color: rgba(0, 0, 0, 0.1);
+                cursor: pointer;
+                transition: background-color 0.25s ease-in-out;
+            }
+
+        `}</style>
+
+        <a href={link}>
+            <button className="icon-button">
+                <img className="icons" src={iconPath}></img>
+                <p><b>{iconName}</b></p>
+            </button>
+        </a>
+        </>
+    );
+}
