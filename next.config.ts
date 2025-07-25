b1531fcd54cd40453e4d3295247152a9ca6b8699
JuanLopez2004 <jlopeze22004@gmail.com>
2025-07-25 17:38:55 -0500

    refactor: config update

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ import { PHASE_PRODUCTION_BUILD } from 'next/constants';
 module.exports = (phase: any, { }) => {
   return {
     output: 'export', // Enables static export
+    trailingSlash: true,  // for file structure
     images: {
       unoptimized: true, // Disable image optimization for static export
     },
